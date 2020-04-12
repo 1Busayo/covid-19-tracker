5157813d413f1aebf8b56359ec1561bd4e743cb3
Busayo Fanimokun <busayo@Busayos-MacBook-Pro.local>
2020-04-12 12:30:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+// import Cards from './components/Cards';
+// import Chart from './components/Chart';
+// import CountryPicker from './components/CountryPicker';
+
+import {Cards,Chart,CountryPicker} from './components';
+import styles from './App.module.css';
+import {fetchData} from './api';
+
+class App extends React.Component {
+state ={
+    data:{},
+}
+
+
+async componentDidMount(){
+    const fetchedData = await fetchData();
+ 
+ this.setState({data:fetchedData});
+}
+
+
+
+    render() {
+        const {data} = this.state;
+        return (
+            <div className={styles.container}>
+               <Cards  data={data}/>
+               <CountryPicker/>
+               <Chart />
+
+            </div>
+        )
+    }
+}
+export default App;
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Charts = () => {
+    return(
+        <h1>Charts</h1>
+    )
+}
+
+export default Charts;
